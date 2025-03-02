ea587bf714bb28d326c6db8ae3c5eab5044f7fb6
VineethReddy <eternalbasic1@gmail.com>
2025-03-02 10:15:43 +0530

    appiled migrations

--- spotterBackend/backend/trips/views.py
+++ spotterBackend/backend/trips/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Trip
+from .serializers import TripSerializer
 
-# Create your views here.
+@api_view(["GET", "POST"])
+def trip_list(request):
+    if request.method == "GET":
+        trips = Trip.objects.all()
+        serializer = TripSerializer(trips, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = TripSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
