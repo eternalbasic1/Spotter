dd8573dd4a21e645ce2a2e9bfbd858cbada40c61
VineethReddy <eternalbasic1@gmail.com>
2025-03-02 10:03:18 +0530

    Updated Model.py

--- spotterBackend/backend/trips/models.py
+++ spotterBackend/backend/trips/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Trip(models.Model):
+    current_location = models.CharField(max_length=255)
+    pickup_location = models.CharField(max_length=255)
+    dropoff_location = models.CharField(max_length=255)
+    cycle_hours_used = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.current_location} to {self.dropoff_location}"
